6c34a179e9b45ad0f6af73b9552a995fbdb7d607
Abhicode873 <148451268+Abhicode873@users.noreply.github.com>
2025-02-06 22:41:55 +0530

    FIRST PROGARM IN PYTHON
    It describes the sum of two numbers average in the Python code

---
+++ firstpro.py
@@ -0,0 +1,23 @@
+# val1 = int(input("eneter the frist num:"))
+# val2 = int(input("eneter the second num :"))
+# sum = val1 + val2
+# print(sum)
+
+# val1 = int(input("eneter the frist num:"))
+# val2 = int(input("eneter the second num :"))
+# print("sum=",val1 + val2)
+
+# #2nd question 
+# a = int(input("eneter the frist side:"))
+
+# print("area of square=",a  * a )
+# print("area of square==",a ** 2)
+
+
+a = float(input("enter the first num:"))
+b = float(input("enter the second num:"))
+print("avg=", (a + b)/2)
+
+a = int(input("enter the first num:"))
+b = int(input("enter the second num:"))
+print(a >=b) 
